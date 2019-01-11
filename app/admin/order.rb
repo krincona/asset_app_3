17ca108f744f0ce6eb3aa632b3366308206d3408
Andy RinGmz <krusworks@gmail.com>
2019-01-11 17:16:31 +0000

    Add discount on Order#Admin Index

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -97,14 +97,18 @@ ActiveAdmin.register Order do
      number_to_currency(o.subsidy,:unit => '$', :precision => 0, :format => '%u%n' )
     end
 =end
+    column "Costo por Hora" do |o|
+      number_to_currency(o.hourly_price, :unit=> '$', :precision => 0, :format => '%u%n')
+    end
+    column "Descuento" do |o|
+     number_to_currency(order.reload.discount,:unit => '$', :precision => 0, :format => '%u%n')
+    end
     column "Costo Total"  do |o|
      number_to_currency(o.sale_price,:unit => '$', :precision => 0, :format => '%u%n' )#(o.monthly_total_cost + o.subsidy_total,:unit => '$', :precision => 0, :format => '%u%n' )
     end
 
-    column "Costo por Hora" do |o|
-      number_to_currency(o.hourly_price, :unit=> '$', :precision => 0, :format => '%u%n')
-    end
-
+    
+    
   end
 
   filter :serial, as: :numeric,:label => 'Consecutivo'
